f49316f5c3f30507d241b3c4dfa12041f56a041c
Ryuhyis <skyblue22756@gmail.com>
2023-03-29 15:47:44 +0900

    최종 db로 api 업데이트

--- api/controller/user.controller.js
+++ api/controller/user.controller.js
@@ -13,7 +13,6 @@ const {
   deleteUser,
   findUserDetailById,
   findLocalById,
-  findSocialById,
   agreeTerm,
   createUserAddress,
   updateUserAddress,
@@ -24,10 +23,16 @@ const {
   findByTermId,
   updateUser,
   updatePwd,
+  createSocialUser,
+  updateExDefaultAddress,
+  updateNewDefaultAddress,
+  findUserProfileById,
+  findUserBasicById,
 } = require("../../database/user.query.js");
 const nodemailer = require("nodemailer");
 require("dotenv").config();
 const smtpTransport = require("nodemailer-smtp-transport");
+const db = require("../../database/pool.js");
 
 //메일 보내는 user 생성
 const transporter = nodemailer.createTransport(
@@ -47,7 +52,7 @@ module.exports = {
   // local login passport 실행후 user 반환
   signIn: async (req, res, next) => {
     try {
-      const user = req.user; // localtbl
+      const user = req.user; // usertbl
       // 로그인 실패시 에러 반환
       if (user.user_id == null) {
         return res.status(user.code).send(new ResponseDto(user.code, user.msg));
@@ -100,7 +105,29 @@ module.exports = {
     }
   },
 
-  // 내 정보 조회 (유저디테일)
+  // 내 정보 조회 (유저)
+  getMyProfile: async (res, req) => {
+    try {
+      const user_id = req.user.id;
+      if (user_id == null) {
+        // usertbl
+        return res
+          .status(req.user.code)
+          .send(new ResponseDto(req.user.code, req.user.msg));
+      }
+
+      // 유저테이블과 조인해서 i/o를 줄이는 쪽으로..
+      const user = await findUserProfileById(user_id);
+      return res
+        .status(200)
+        .send(new ResponseDto(200, "내 정보 조회 성공", req.user));
+    } catch (err) {
+      console.log(err);
+      return res.status(500).send(err.message);
+    }
+  },
+
+  // 내 상세 정보 조회 (유저디테일) - 이름, 닉네임, 핸드폰번호, 성별, 생년월일
   getMyDetail: async (res, req) => {
     try {
       const user_id = req.user.id; // usertbl
@@ -119,29 +146,24 @@ module.exports = {
       return res.status(500).send(err.message);
     }
   },
-  // 내 계정 조회 (로컬, 소셜)
-  getMyAccount: async (res, req) => {
+
+  // 프로필 이미지, 닉네임
+  getMyBasicInfo: async (res, req) => {
     try {
-      const user_id = req.user.id; // usertbl
+      const user_id = req.user.id;
       if (user_id == null) {
+        // usertbl
         return res
           .status(req.user.code)
           .send(new ResponseDto(req.user.code, req.user.msg));
       }
-      // 로컬 유저면
-      if (req.user.login_type == "LOCAL") {
-        const localUser = await findLocalById(user_id);
-        return res
-          .status(200)
-          .send(new ResponseDto(200, "내 계정 조회 성공", localUser));
-      }
 
-      if (req.user.login_type == "SOCIAL") {
-        const socialUser = await findSocialById(user_id);
-        return res
-          .status(200)
-          .send(new ResponseDto(200, "내 계정 조회 성공", socialUser));
-      }
+      const user = await findUserBasicById(user_id);
+      return res
+        .status(200)
+        .send(
+          new ResponseDto(200, "프로필이미지, 닉네임 정보 조회 성공", user)
+        );
     } catch (err) {
       console.log(err);
       return res.status(500).send(err.message);
@@ -163,25 +185,6 @@ module.exports = {
       return res
         .status(200)
         .send(new ResponseDto(200, "내 약관 동의 수정 성공"));
-    } catch (err) {
-      console.log(err);
-      return res.status(500).send(err.message);
-    }
-  },
-
-  // 내 프로필 조회 (유저)
-  getMyProfile: async (res, req) => {
-    try {
-      const user_id = req.user.id;
-      if (user_id == null) {
-        // usertbl
-        return res
-          .status(req.user.code)
-          .send(new ResponseDto(req.user.code, req.user.msg));
-      }
-      return res
-        .status(200)
-        .send(new ResponseDto(200, "내 정보 조회 성공", req.user));
     } catch (err) {
       console.log(err);
       return res.status(500).send(err.message);
@@ -210,6 +213,12 @@ module.exports = {
     }
   },
 
+  // 배송지 id로 조회
+  getUserAddressById: async (req, res) => {
+    try {
+    } catch (error) {}
+  },
+
   // 배송지 등록
   createUserAddress: async (req, res) => {
     try {
@@ -219,6 +228,12 @@ module.exports = {
         return res
           .status(req.user.code)
           .send(new ResponseDto(req.user.code, req.user.msg));
+      }
+
+      // 기본 배송지로 저장 체크했는지 확인
+      if (req.body.status == 1) {
+        // 기존 배송지를 찾아서 일반으로 바꿈
+        await updateExDefaultAddress(user_id);
       }
 
       // 주소 삽입
@@ -247,6 +262,12 @@ module.exports = {
           .send(new ResponseDto(404, "해당 배송지가 없습니다"));
       }
 
+      // 일반배송지 -> 기본배송지
+      if (req.body.status == 1 && isExist.status == 0) {
+        // 기존 배송지를 찾아서 일반으로 바꿈
+        await updateExDefaultAddress(user_id);
+      }
+
       // 주소 수정
       await updateUserAddress(id, req.body);
       return res.status(200).send(new ResponseDto(200, "배송지 수정 성공"));
@@ -271,6 +292,12 @@ module.exports = {
         return res
           .status(404)
           .send(new ResponseDto(404, "해당 배송지가 없습니다"));
+      }
+
+      // 기본 배송지 삭제하는 경우
+      if (isExist.status == 1) {
+        // 제일 최근 수정 배송지를 기본으로 등록
+        await updateNewDefaultAddress(address_id);
       }
 
       await deleteUserAddress(address_id, user_id);
@@ -522,6 +549,35 @@ module.exports = {
       return res.status(500).json(err.message);
     }
   },
+
+  // // 내 계정 조회 (로컬, 소셜)
+  // getMyAccount: async (res, req) => {
+  //   try {
+  //     const user_id = req.user.id; // usertbl
+  //     if (user_id == null) {
+  //       return res
+  //         .status(req.user.code)
+  //         .send(new ResponseDto(req.user.code, req.user.msg));
+  //     }
+  //     // 로컬 유저면
+  //     if (req.user.login_type == "LOCAL") {
+  //       const localUser = await findLocalById(user_id);
+  //       return res
+  //         .status(200)
+  //         .send(new ResponseDto(200, "내 계정 조회 성공", localUser));
+  //     }
+
+  //     if (req.user.login_type == "SOCIAL") {
+  //       const socialUser = await findSocialById(user_id);
+  //       return res
+  //         .status(200)
+  //         .send(new ResponseDto(200, "내 계정 조회 성공", socialUser));
+  //     }
+  //   } catch (err) {
+  //     console.log(err);
+  //     return res.status(500).send(err.message);
+  //   }
+  // },
 };
 
 // 토큰 만들기
--- database/pool.js
+++ database/pool.js
@@ -6,7 +6,7 @@ const pool = mysql.createPool({
   port: 3306,
   user: "root",
   password: "1234",
-  database: "memberdb",
+  database: "memberdb2",
 });
 
 const db = pool.promise();
