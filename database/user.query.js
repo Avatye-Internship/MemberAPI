6fc63669691f2ce7cf48c234dbc3d80ea5291c39
nkyungm <skarudals27@naver.com>
2023-03-29 15:51:44 +0900

    code 수정

--- database/user.query.js
+++ database/user.query.js
@@ -1,44 +1,46 @@
 const db = require("./pool");
 const bcrypt = require("bcrypt");
+
 //회원가입
 exports.createLocalUser = async (users, terms) => {
   const {
-    nickname,
-    profile_img,
     email,
     pwd,
-    role,
     name,
-    phone,
     gender,
     birth,
+    nickname,
+    profile_img
   } = users;
   const hashed = await bcrypt.hash(pwd, 10);
-  const insertId = await db
-    .query(
-      "insert into userstbl(nickname,profile_img,grade_id,login_type) value(?,?,1,'LOCAL')",
-      [nickname, profile_img]
-    )
+  const conn = await pool.getConnection(); //트랜잭션 연결
+  try{
+    await conn.beginTransaction(); // 트랜잭션 적용 시작
+    
+    const insertId = await db.query("insert into userstbl(email,pwd) value(?,?)",[email, hashed])
     .then((data) => {
       return data[0].insertId;
     });
 
   await db.query(
-    "insert into localtbl(user_id, email, pwd, role) value(?,?,?,?)",
-    [insertId, email, hashed, role]
-  );
-
-  await db.query(
-    "insert into User_Detailstbl(user_id,name,phone,gender,birth) value(?,?,?,?,?)",
-    [insertId, name, phone, gender, birth]
+    "insert into User_Detailstbl(user_id,name,gender,birth,nickname,profile_img) value(?,?,?,?,?,?)",
+    [insertId, name, gender, birth,nickname,profile_img]
   );
 
   await this.termsIsRequired(terms, insertId);
+  await conn.commit() // 커밋
 
   return insertId;
+  }catch(err){
+    console.log(err)
+    await conn.rollback() // 롤백
+    return res.status(500).json(err)
+  } finally {
+    conn.release() // conn 회수
+  }
 };
 
-//약관동의 등록 - 경민
+//약관동의 등록 
 exports.termsIsRequired = async (terms, userId) => {
   let terms_register_query = `INSERT INTO user_termtbl (term_id,isAgree,user_id) VALUES`;
 
@@ -53,62 +55,63 @@ exports.termsIsRequired = async (terms, userId) => {
   await db.query(terms_register_query);
 };
 
-//이메일 조회 - 경민
+//이메일 조회 
 exports.findByEmail = async (email) => {
   return db
-    .query("select * from localtbl where email=?", [email])
+    .query("select * from userstbl where email=?", [email])
     .then((data) => data[0][0]);
 };
 
-//이메일 유효성 검사(인증코드 전송) - 경민
-exports.validByEmail = async (localId) => {
-  //이메일 인증코드 db에 저장
-  const verificationCode = generateVerificationCode(); //인증코드 생성
-  const insertId = await db
-    .query("insert into Emailcodestbl(local_id,verification_code) value(?,?)", [
-      localId,
-      verificationCode,
-    ])
-    .then((data) => {
-      return data[0].insertId;
-    });
-  return { insertId, verificationCode };
-};
+// //이메일 유효성 검사(인증코드 전송) 
+// exports.validByEmail = async (userId) => {
+//   //이메일 인증코드 db에 저장
+//   const verificationCode = generateVerificationCode(); //인증코드 생성
+//   const insertId = await db
+//     .query("insert into Emailcodestbl(user_id,verification_code) value(?,?)", [
+//       userId,
+//       verificationCode,
+//     ])
+//     .then((data) => {
+//       return data[0].insertId;
+//     });
+//   return { insertId, verificationCode };
+// };
 
-//이메일 인증코드 확인 - 경민
-exports.findByVerificationCode = async (email) => {
-  const EmailCode = await db.query(
-    "select * from Emailcodestbl join Localtbl on Localtbl.id=Emailcodestbl.local_id where Localtbl.email=? order by Emailcodestbl.created_at DESC",
-    [email]
-  );
-  return EmailCode[0][0];
-};
+//이메일 인증코드 확인 
+// exports.findByVerificationCode = async (email) => {
+//   const EmailCode = await db.query(
+//     "select * from Emailcodestbl join Localtbl on Localtbl.id=Emailcodestbl.local_id where Localtbl.email=? order by Emailcodestbl.created_at DESC",
+//     [email]
+//   );
+//   return EmailCode[0][0];
+// };
 
-//이메일 인증코드 삭제 - 경민
-exports.deleteEmailCode = async (email) => {
-  return await db.query(
-    "delete Emailcodestbl from Emailcodestbl join Localtbl on Localtbl.id=Emailcodestbl.local_id where Localtbl.email=?",
-    [email]
-  );
-};
+// //이메일 인증코드 삭제 
+// exports.deleteEmailCode = async (email) => {
+//   return await db.query(
+//     "delete Emailcodestbl from Emailcodestbl join Localtbl on Localtbl.id=Emailcodestbl.local_id where Localtbl.email=?",
+//     [email]
+//   );
+// };
 
-//내 프로필 수정(Userstbl)- 경민
-exports.updateUser = async (id, user) => {
-  const { nickname, profile_img } = user;
-  return db
-    .query("update userstbl set nickname=?, profile_img=? where id=?", [
-      nickname,
-      profile_img,
-      id,
-    ])
-    .then((data) => {
-      return data[0];
-    });
-};
-//내 정보 수정(UserDetailstbl) -경민
+//내 프로필 수정(Userstbl)
+// exports.updateUser = async (id, user) => {
+//   const { nickname, profile_img } = user;
+//   return db
+//     .query("update userstbl set nickname=?, profile_img=? where id=?", [
+//       nickname,
+//       profile_img,
+//       id,
+//     ])
+//     .then((data) => {
+//       return data[0];
+//     });
+// };
+
+//내 정보 수정(UserDetailstbl)
 exports.updateUserDetails = async (id, users) => {
   let user_update_query = `UPDATE User_Detailstbl SET `;
-  //console.log(Object.keys(users));
+
   for (i = 0; i < Object.keys(users).length; i++) {
     user_update_query += `${Object.keys(users)[i]} = '${
       Object.values(users)[i]
@@ -116,63 +119,27 @@ exports.updateUserDetails = async (id, users) => {
   }
   user_update_query = user_update_query.slice(0, -1);
   user_update_query += ` WHERE user_id = '${id}';`;
-  console.log(user_update_query);
+  //console.log(user_update_query);
 
   return db.query(user_update_query).then((data) => {
     return data[0];
   });
 };
-//비밀번호 변경 - 경민
+//비밀번호 변경 
 exports.updatePwd = async (id, pwd) => {
   const hashed = await bcrypt.hash(pwd, 10);
   return await db
-    .query("update localtbl set pwd=? where user_id=?", [hashed, id])
+    .query("update Userstbl set pwd=? where user_id=?", [hashed, id])
     .then((data) => {
       return data[0];
     });
 };
-//회원 탈퇴 - 경민
-exports.deleteUser = async (id, reason_text) => {
-  let accountId;
-  const localType = await db
-    .query("select login_type from userstbl where id=?", [id])
-    .then((data) => {
-      return data[0][0];
-    });
-  //console.log(localType.login_type);
-  if (localType.login_type == "LOCAL") {
-    account_id = await db
-      .query("select email from localtbl where user_id=?", [id])
-      .then((data) => {
-        return data[0][0];
-      });
-    accountId = account_id.email;
-  } else {
-    account_id = await db
-      .query("select sns_id from socialtbl where user_id=?", [id])
-      .then((data) => {
-        return data[0][0];
-      });
-    accountId = account_id.sns_id;
-  }
+//회원 탈퇴 
+exports.deleteUser = async (id) => {
 
-  const phone = await db
-    .query("select phone from User_Detailstbl where user_id=?", [id])
-    .then((data) => {
-      return data[0][0];
-    });
-  //console.log(phone.phone);
-
-  const WithdrawlId = await db
-    .query(
-      "insert into Withdrawltbl(user_id,account_id,phone,reason_text) values(?,?,?,?)",
-      [id, accountId, phone.phone, reason_text]
-    )
-    .then((data) => {
-      return data[0].insertId;
-    });
-
-  await db.query("delete from Userstbl where id=?", [id]);
+  return db
+    .query("UPDATE Userstbl set active=0, deleted_at=NOW() where user_id=?",[id])
+    .then((data) => data[0][0]);
 };
 
 exports.findAllUser = async () => {
@@ -186,18 +153,18 @@ exports.findAllUser = async () => {
 
 exports.findById = async (id) => {
   return db
-    .query("select * from userstbl where id=?", [id])
+    .query("select * from userstbl where user_id=?", [id])
     .then((data) => data[0][0]);
 };
 
-exports.findLocalById = async (id) => {
-  return db
-    .query(
-      "select id, user_id, email, created_at, role,pwd from localtbl where user_id=?",
-      [id]
-    )
-    .then((data) => data[0][0]);
-};
+// exports.findLocalById = async (id) => {
+//   return db
+//     .query(
+//       "select id, user_id, email, created_at, role,pwd from localtbl where user_id=?",
+//       [id]
+//     )
+//     .then((data) => data[0][0]);
+// };
 
 exports.findSocialById = async (id) => {
   return db
@@ -387,13 +354,3 @@ exports.agreeTerm = async (id, isAgree, user_id) => {
   ]);
 };
 
-//인증코드 생성
-const generateVerificationCode = () => {
-  const chars =
-    "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-  let code = "";
-  for (let i = 0; i < 6; i++) {
-    code += chars[Math.floor(Math.random() * chars.length)];
-  }
-  return code;
-};
