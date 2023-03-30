31320d81006e2df26d069e99d1a0f2573af794b2
nkyungm <skarudals27@naver.com>
2023-03-30 18:04:25 +0900

    ts+restify 변경

--- database/user.query.ts
+++ database/user.query.ts
@@ -1,10 +1,16 @@
 import bcrypt from "bcrypt";
-import db from "./db";
+import Address from "../api/model/Address";
+import { SignUpDto, SocialLoginDto } from "../api/model/ReqDto";
+import Terms from "../api/model/Terms";
+import { Role } from "../api/model/Users";
+import User_Details from "../api/model/User_Details";
+import User_Term from "../api/model/User_Term";
+import db from "./pool";
 
 class UserQuery {
   //회원가입
-  public async createLocalUser(users, terms) {
-    const { email, pwd, name, gender, birth, nickname, profile_img } = users;
+  public async createLocalUser(signupDto: SignUpDto) {
+    const { email, pwd, name, gender, birth, nickname, profile_img,terms } = signupDto;
     const hashed = await bcrypt.hash(pwd, 10);
     const conn = await db.getConnection(); //트랜잭션 연결
     try {
@@ -12,7 +18,7 @@ class UserQuery {
 
       const insertId = await db
         .query("insert into userstbl(email,pwd) value(?,?)", [email, hashed])
-        .then((data) => {
+        .then((data:any) => {
           return data[0].insertId;
         });
 
@@ -35,13 +41,13 @@ class UserQuery {
   }
 
   //약관동의 등록
-  public async termsIsRequired(terms, userId) {
+  public async termsIsRequired(terms:User_Term[], userId:number) {
     let terms_register_query = `INSERT INTO user_termtbl (term_id,isAgree,user_id) VALUES`;
 
     //약관동의별 insert 쿼리문 추가
     for (let i = 0; i < terms.length; i++) {
       //약관동의 필수 여부에 따른 에러
-      terms_register_query += `('${terms[i].term_id}','${terms[i].isAgree}','${userId}'),`;
+      terms_register_query += `('${terms[i].term_id}','${terms[i].is_agree}','${userId}'),`;
     }
     terms_register_query =
       terms_register_query.substring(0, terms_register_query.length - 1) + ";";
@@ -50,10 +56,10 @@ class UserQuery {
   }
 
   //이메일 조회
-  public async findByEmail(email) {
+  public async findByEmail(email:string) {
     return db
       .query("select * from userstbl where email=? and active=1", [email])
-      .then((data) => data[0][0]);
+      .then((data:any) => data[0][0]);
   }
 
   // //이메일 유효성 검사(인증코드 전송)
@@ -103,10 +109,10 @@ class UserQuery {
   // };
 
   //내 정보 수정(UserDetailstbl)
-  public async updateUserDetails(id, users) {
+  public async updateUserDetails(id:number, users:User_Details) {
     let user_update_query = `UPDATE User_Detailstbl SET `;
 
-    for (i = 0; i < Object.keys(users).length; i++) {
+    for (let i = 0; i < Object.keys(users).length; i++) {
       user_update_query += `${Object.keys(users)[i]} = '${
         Object.values(users)[i]
       }',`;
@@ -115,26 +121,26 @@ class UserQuery {
     user_update_query += ` WHERE user_id = '${id}';`;
     //console.log(user_update_query);
 
-    return db.query(user_update_query).then((data) => {
+    return db.query(user_update_query).then((data:any) => {
       return data[0];
     });
   }
   //비밀번호 변경
-  public async updatePwd(id, pwd) {
+  public async updatePwd(id:number, pwd:string) {
     const hashed = await bcrypt.hash(pwd, 10);
     return await db
       .query("update Userstbl set pwd=? where user_id=?", [hashed, id])
-      .then((data) => {
+      .then((data:any) => {
         return data[0];
       });
   }
   //회원 탈퇴
-  public async deleteUser(id) {
+  public async deleteUser(id:number) {
     return db
       .query("UPDATE Userstbl set active=0, deleted_at=NOW() where user_id=?", [
         id,
       ])
-      .then((data) => data[0][0]);
+      .then((data:any) => data[0][0]);
   }
 
   public async findAllUser() {
@@ -145,10 +151,10 @@ class UserQuery {
     return users;
   }
 
-  public async findById(id) {
+  public async findById(id:number) {
     return db
       .query("select * from userstbl where user_id=?", [id])
-      .then((data) => data[0][0]);
+      .then((data:any) => data[0][0]);
   }
 
   // public async .findLocalById = async (id) => {
@@ -160,45 +166,45 @@ class UserQuery {
   //     .then((data) => data[0][0]);
   // };
 
-  public async findSocialById(id) {
+  public async findSocialById(id:number) {
     return db
       .query("select * from socialtbl where user_id=?", [id])
-      .then((data) => data[0][0]);
+      .then((data:any) => data[0][0]);
   }
 
-  public async findUserDetailById(id) {
+  public async findUserDetailById(id:number) {
     return db
       .query("select * from user_detailstbl where user_id=?", [id])
-      .then((data) => data[0][0]);
+      .then((data:any) => data[0][0]);
   }
 
-  public async findUserBasicById(id) {
+  public async findUserBasicById(id:number) {
     return db
       .query("select nickname, profile_img from userstbl where id=?", [id])
-      .then((data) => data[0][0]);
+      .then((data:any) => data[0][0]);
   }
 
   // user, user_detail, address 모두 조회 후 반환
-  public async findUserInfoById(id) {
+  public async findUserInfoById(id:number) {
     return db
       .query(
         "select * from userstbl u inner join user_detailstbl ud on u.id = ud.user_id inner join addresstbl ad on ud.user_id = ad.user_id",
         [id]
       )
-      .then((data) => data[0][0]);
+      .then((data:any) => data[0][0]);
   }
 
-  public async findUserProfileById(id) {
+  public async findUserProfileById(id:number) {
     // 등급, 이메일, 로그인타입, 포인트, 프로필이미지, 닉네임
     return db
       .query(
         "select u.id, u.user_grade_id, u.email, u.login_type, ud.point, ud.profile_img, ud.nickname from userstbl u inner join user_detailtbl ud on u.id = ud.user_id",
         [id]
       )
-      .then((data) => data[0][0]);
+      .then((data:any) => data[0][0]);
   }
 
-  public async createSocialUser(users) {
+  public async createSocialUser(users:SocialLoginDto) {
     const { login_type, email, open_id, nickname } = users;
 
     const conn = await db.getConnection();
@@ -210,7 +216,7 @@ class UserQuery {
           "insert into userstbl(user_grade_id, login_type, email) value(3,?, ?)",
           [login_type, email]
         )
-        .then((data) => {
+        .then((data:any) => {
           return data[0].insertId;
         });
       // social tbl
@@ -238,19 +244,19 @@ class UserQuery {
     }
   }
 
-  public async termsIsRequiredSocial(userId) {
+  public async termsIsRequiredSocial(userId:number) {
     let terms_register_query = `INSERT INTO user_termtbl (term_id,isAgree,created_at,user_id,updated_at) VALUES`;
     // 필수 약관만 뽑아오기
     const term_result = await db
       .query("select id from Termstbl where isRequired=true")
-      .then((data) => {
+      .then((data:any) => {
         return data[0];
       });
     //약관동의별 insert 쿼리문 추가
     for (let i = 0; i < term_result.length; i++) {
       //약관동의 필수 여부에 따른 에러
       i;
-      terms_register_query += `('${terms_result[i].id}','true',NOW(),'${userId}',NOW()),`;
+      terms_register_query += `('${term_result[i].id}','true',NOW(),'${userId}',NOW()),`;
     }
     terms_register_query =
       terms_register_query.substring(0, terms_register_query.length - 1) + ";";
@@ -258,47 +264,47 @@ class UserQuery {
   }
 
   //
-  public async updateUserRole(id, role) {
+  public async updateUserRole(id:number, role:Role) {
     return db
       .query("update userstbl set role=? where id=?", [role, id])
-      .then((data) => {
+      .then((data:any) => {
         return data[0];
       });
   }
 
   //
-  public async findBySocialId(id, provider) {
-    return db
-      .query("select * from socialtbl where sns_id=? and provider=?", [
-        id,
-        login_type,
-      ])
-      .then((data) => {
-        return data[0][0];
-      });
-  }
+  // public async findBySocialId(id:number, provider) {
+  //   return db
+  //     .query("select * from socialtbl where sns_id=? and provider=?", [
+  //       id,
+  //       login_type,
+  //     ])
+  //     .then((data) => {
+  //       return data[0][0];
+  //     });
+  // }
   //
-  public async findAllUserAddress(id) {
+  public async findAllUserAddress(id:number) {
     return db
       .query("select * from addresstbl where user_id=?", [id])
-      .then((data) => {
+      .then((data:any) => {
         return data[0][0];
       });
   }
   //
-  public async findUserAddressById(address_id, user_id) {
+  public async findUserAddressById(address_id:number, user_id:number) {
     return db
       .query("select * from addresstbl where address_id=? and user_id=?", [
         address_id,
         user_id,
       ])
-      .then((data) => {
+      .then((data:any) => {
         return data[0][0];
       });
   }
 
   // 기존 기본배송지 -> 일반 배송지
-  public async updateExDefaultAddress(user_id) {
+  public async updateExDefaultAddress(user_id:number) {
     const conn = await db.getConnection();
     try {
       await conn.beginTransaction();
@@ -321,7 +327,7 @@ class UserQuery {
   }
 
   // 제일 최근 배송지 -> 기본 배송지
-  public async updateNewDefaultAddress(user_id) {
+  public async updateNewDefaultAddress(user_id:number) {
     const conn = await db.getConnection();
     try {
       await conn.beginTransaction();
@@ -343,7 +349,7 @@ class UserQuery {
     }
   }
 
-  public async createUserAddress(address_request) {
+  public async createUserAddress(address_request:Address) {
     const {
       zip_code,
       address,
@@ -364,12 +370,12 @@ class UserQuery {
         receiver_name,
         receiver_phone,
       ])
-      .then((data) => {
+      .then((data:any) => {
         return data[0].insertId;
       });
   }
   //
-  public async updateUserAddress(id, address_request) {
+  public async updateUserAddress(id:number, address_request:Address) {
     const { zip_code, address, address_detail, request_msg, status } =
       address_request;
 
@@ -378,61 +384,61 @@ class UserQuery {
         "update userstbl set zip_code=?, address=?, address_detail=?, request_msg=?, status=? where user_id=?",
         [zip_code, address, address_detail, request_msg, status, id]
       )
-      .then((data) => {
+      .then((data:any) => {
         return data[0].updateId;
       });
   }
   //
-  public async deleteUserAddress(address_id, user_id) {
+  public async deleteUserAddress(address_id:number, user_id:number) {
     await db.query("delete from addresstbl where id=? and user_id=?", [
       address_id,
       user_id,
     ]);
   }
 
-  public async findAllUserTerms(id) {
+  public async findAllUserTerms(id:number) {
     return db
       .query("select * from user_termtbl where user_id=? ", [id])
-      .then((data) => data[0]);
+      .then((data:any) => data[0]);
   }
 
-  public async findByTermId(user_id, term_id) {
+  public async findByTermId(user_id:number, term_id:number) {
     return db
       .query("select * from user_termtbl where user_id=? and term_code=?", [
         user_id,
         term_id,
       ])
-      .then((data) => data[0][0]);
+      .then((data:any) => data[0][0]);
   }
 
-  public async createTerm(term) {
-    const { termName, termContent, isRequired } = term;
+  public async createTerm(term:Terms) {
+    const { name, is_required } = term;
     return db
       .query(
-        "insert into term(termName, termContent, isRequired) values(?,?,?)",
-        [termName, termContent, isRequired]
+        "insert into termstbl(name,is_required) values(?,?)",
+        [name,is_required]
       )
-      .then((data) => {
+      .then((data:any) => {
         return data[0].insertId;
       });
   }
 
-  public async createUserTerm(id, termName, isAgree) {
-    return db
-      .query("insert into userterm(userId, termName, isAgree) values(?,?,?)", [
-        id,
-        termName,
-        isAgree,
-      ])
-      .then((data) => {
-        console.log(data[0].insertId);
-        return data[0].insertId;
-      });
-  }
+  // public async createUserTerm(id:number, termName:string, isAgree:boolean) {
+  //   return db
+  //     .query("insert into userterm(userId, termName, isAgree) values(?,?,?)", [
+  //       id,
+  //       termName,
+  //       isAgree,
+  //     ])
+  //     .then((data:any) => {
+  //       console.log(data[0].insertId);
+  //       return data[0].insertId;
+  //     });
+  // }
 
-  public async agreeTerm(id, isAgree, user_id) {
+  public async agreeTerm(id:number, isAgree:boolean, user_id:number) {
     await db.query(
-      "update user_termtbl set isAgree=? where id=? and userId=?",
+      "update user_termtbl set is_agree=? where term_id=? and user_id=?",
       [isAgree, id, user_id]
     );
   }
