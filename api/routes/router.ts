05e32d1b1748f3a378c24e66c7286e5466cb5f82
Ryuhyis <skyblue22756@gmail.com>
2023-03-31 10:32:01 +0900

    restify router 등록 실패

--- api/routes/router.ts
+++ api/routes/router.ts
@@ -3,7 +3,7 @@ import { userController } from "../controller/user.controller";
 import { adminController } from "../controller/admin.controller";
 import { Router } from "restify-router";
 
-var router = new Router();
+var userRouter = new Router();
 
 const requireUserSignIn = passport.authenticate("local-user", {
   session: false,
@@ -24,34 +24,38 @@ const requireAdminAuth = passport.authenticate("jwt-admin", {
 
 const requireKakao = passport.authenticate("kakao", { session: false });
 
-router.get("/test", function (req, res, next) {});
+userRouter.get("/test", requireAdminAuth, function (req, res, next) {});
 
-router.post("/test", function (req, res, next) {});
+userRouter.post("/test", requireAdminAuth, function (req, res, next) {});
 
 /*
     관리자 API
   */
-router.post("/admin/login", requireAdminSignIn, adminController.adminSignIn);
+userRouter.post(
+  "/admin/login",
+  requireAdminSignIn,
+  adminController.adminSignIn
+);
 
 // 전체 회원 조회 admin
-router.get("/admin/users", requireAdminAuth, adminController.getUsers);
+userRouter.get("/admin/users", requireAdminAuth, adminController.getUsers);
 
 // 회원 계정 조회
-router.get(
+userRouter.get(
   "/admin/users/account/:id",
   requireAdminAuth,
   adminController.getUserAccount
 );
 
 // 회원 정보 상세 조회
-router.get(
+userRouter.get(
   "/admin/users/info/:id",
   requireAdminAuth,
   adminController.getUserInfo
 );
 
 // 회원 권한 수정
-router.patch(
+userRouter.patch(
   "/admin/users/role/:id",
   requireAdminAuth,
   adminController.updateUserRole
@@ -61,70 +65,93 @@ router.patch(
     회원 API
   */
 // 회원가입
-router.post("/users", userController.signUp);
+userRouter.post("/users", userController.signUp);
 
 //이메일 유효성 인증 (회원가입 시)
-router.get("/users/check/email", userController.emailValid_signUp);
+userRouter.get("/users/check/email", userController.emailValid_signUp);
 //이메일 유효성 인증 (비밀번호 찾기 시)
-router.get("/users/check/email-pwd", userController.emailValid_updatePwdByDB);
+userRouter.get(
+  "/users/check/email-pwd",
+  userController.emailValid_updatePwdByDB
+);
 
 // //이메일 인증코드 확인
-// router.get("/users/check/email", userController.emailcodeCheck);
+// userRouter.get("/users/check/email", userController.emailcodeCheck);
 // //이메일 인증코드 확인
-// router.delete("/users/check/email", userController.emailcodeDelete);
+// userRouter.delete("/users/check/email", userController.emailcodeDelete);
 
 // //내 프로필 수정(Userstbl)
-// router.patch("/users/profile", requireUserAuth, userController.updateMyUsers);
+// userRouter.patch("/users/profile", requireUserAuth, userController.updateMyUsers);
 
 //내 정보 수정(UserDetailstbl)
-router.patch("/users", requireUserAuth, userController.updateMyUserDetails);
+userRouter.patch("/users", requireUserAuth, userController.updateMyUserDetails);
 
 //비밀번호 변경
-router.patch(
+userRouter.patch(
   "/users/change/pwd",
   requireUserAuth,
   userController.updatePwdByLogin
 );
 //비밀번호 찾기
-router.patch("/users/find/pwd", userController.updatePwdByDB);
+userRouter.patch("/users/find/pwd", userController.updatePwdByDB);
 
 //회원탈퇴
-router.post("/users/account", requireUserAuth, userController.deleteUser);
+userRouter.post("/users/account", requireUserAuth, userController.deleteUser);
 
 // 로그인
-router.post("/users/login/local", requireUserSignIn, userController.signIn);
+userRouter.post("/users/login/local", requireUserSignIn, userController.signIn);
 // 소셜로그인 (카카오 엔드포인트, 콜백)
-router.get("/users/login/kakao", requireKakao);
-router.get(
+userRouter.get("/users/login/kakao", requireKakao);
+userRouter.get(
   "/users/login/kakao/callback",
   requireKakao,
   userController.socialLogin
 );
 // 이메일 중복 확인
-//router.post("/users/check/email", userController.checkEmail);
+//userRouter.post("/users/check/email", userController.checkEmail);
 // 내 정보 조회
-router.get("/users", requireUserAuth, userController.getMyDetail);
+userRouter.get("/users", requireUserAuth, userController.getMyDetail);
 // 내 프로필 조회 (이메일, 프로필이미지, 닉네임, 등급, 로그인타입, ..)
-router.get("/users/profile", requireUserAuth, userController.getMyProfile);
+userRouter.get("/users/profile", requireUserAuth, userController.getMyProfile);
 // 닉네임, 프로필 이미지만 조회
-router.get("users/basic-info", requireUserAuth, userController.getMyBasicInfo);
+userRouter.get(
+  "users/basic-info",
+  requireUserAuth,
+  userController.getMyBasicInfo
+);
 // 내 주소 등록
-router.post(
+userRouter.post(
   "/users/address",
   requireUserAuth,
   userController.createUserAddress
 );
 // 내 주소 조회
-router.get("/users/address", requireUserAuth, userController.getMyBasicInfo);
+userRouter.get(
+  "/users/address",
+  requireUserAuth,
+  userController.getMyBasicInfo
+);
 // 내 주소 수정
-router.put("/users/address", requireUserAuth, userController.updateUserAddress);
+userRouter.put(
+  "/users/address",
+  requireUserAuth,
+  userController.updateUserAddress
+);
 // 내 주소 삭제
-router.del("/users/address", requireUserAuth, userController.deleteUserAddress);
+userRouter.del(
+  "/users/address",
+  requireUserAuth,
+  userController.deleteUserAddress
+);
 // 약관 동의 (수정)
-router.patch("/users/terms/:id", requireUserAuth, userController.updateTerm);
+userRouter.patch(
+  "/users/terms/:id",
+  requireUserAuth,
+  userController.updateTerm
+);
 // 약관 전체에 대해 사용자 동의 여부 조회
-router.get("/terms", requireUserAuth, userController.getTerms);
+userRouter.get("/terms", requireUserAuth, userController.getTerms);
 // 약관 코드별로 사용자 동의 여부 조회
-router.get("/terms/:code", requireUserAuth, userController.getTerm);
+userRouter.get("/terms/:code", requireUserAuth, userController.getTerm);
 
-export { router };
+export default userRouter;
--- index.ts
+++ index.ts
@@ -1,17 +1,31 @@
 import * as restify from "restify";
 import passport from "passport";
-import { router } from "./api/routes/router";
 import { Router } from "restify-router";
 
-// 서버 생성
+import userRouter from "./api/routes/router";
+import corsMiddleware from "restify-cors-middleware2";
 
+// 서버 생성
+const router = new Router();
+
+const cors = corsMiddleware({
+  preflightMaxAge: 5,
+  origins: [],
+  allowHeaders: ["Authorization"],
+  exposeHeaders: ["Authorization"],
+});
 var server = restify.createServer();
-router.applyRoutes(server);
-server.use(restify.plugins.conditionalRequest);
+server.pre(cors.preflight);
+server.use(cors.actual);
+
 server.use(restify.plugins.bodyParser({ requestBodyOnGet: true }));
 server.use(restify.plugins.queryParser());
 server.use(restify.plugins.acceptParser(server.acceptable));
 server.use(restify.plugins.urlEncodedBodyParser());
+
+router.add("/api", userRouter);
+router.applyRoutes(server);
+
 passport.initialize();
 server.listen(3000, function () {
   console.log("server on", server.url);
