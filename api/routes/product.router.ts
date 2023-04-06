6599a30a29332c60bb2297654f2c22013127e770
nkyungm <skarudals27@naver.com>
2023-04-06 17:21:08 +0900

    product api

--- api/routes/product.router.ts
+++ api/routes/product.router.ts
@@ -1,10 +1,16 @@
-import express from "express";
-import productController from "../controller/product.controller";
+import express from 'express';
+import productController from '../controller/product.controller';
+import { requireUserAuth } from '../passport/passport';
 
 const productRouter = express.Router();
 
-productRouter.get("/product", productController.findProduct);
+// productRouter.get('/product', productController.findProduct);
 
-productRouter.get("/product/:id", productController.findProductById);
+// productRouter.get('/product/:id', productController.findProductById);
+
+// 참여 상태(status) 수정 및 포인트 적립
+productRouter.patch('/product/ads/:id/user-status', requireUserAuth, productController.updatePointByStatus);
+
+// 등급 수정
 
 export default productRouter;
--- database/product.query.ts
+++ database/product.query.ts
@@ -1,4 +1,4 @@
-import pool from "./pool";
+import pool from './pool';
 
 class ProductQuery {
   private pool = pool;
@@ -9,7 +9,7 @@ class ProductQuery {
     try {
       // const sql = 'select product_id, name, description, product_point,
       // category,start_date, end_date from Product ';
-      await this.db.query("call ", [category]);
+      await this.db.query('call ', [category]);
     } catch (error) {
       console.log(error);
     }
@@ -17,7 +17,16 @@ class ProductQuery {
 
   public async findProductById(id: string) {
     try {
-      await this.db.query("call", [id]);
+      await this.db.query('call', [id]);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  public async updatePointByStatus(id: string, product_id: string): Promise<void> {
+    try {
+      const sql = 'call update_user_status (?,?)';
+      await this.db.query(sql, [id, product_id]);
     } catch (error) {
       console.log(error);
     }
