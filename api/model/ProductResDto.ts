ad248a13cfcfe162252e4b8b30aab42bfb8299d2
Ryuhyis <skyblue22756@gmail.com>
2023-04-19 17:38:20 +0900

    cookie 서버에서 세팅

--- api/model/ProductResDto.ts
+++ api/model/ProductResDto.ts
@@ -13,13 +13,25 @@ class ProductResDto {
 
   status;
 
+  main_img;
+
+  detail_img;
+
+  start_at;
+
+  end_at;
+
   constructor(
     product_id: number,
     name: string,
     description: string,
     product_point: number,
     category: Category,
+    main_img: string,
     status?: string,
+    detail_img?: string,
+    start_at?: Date,
+    end_at?: Date,
   ) {
     this.product_id = product_id;
     this.name = name;
@@ -27,6 +39,10 @@ class ProductResDto {
     this.product_point = product_point;
     this.category = category;
     this.status = status;
+    this.main_img = main_img;
+    this.detail_img = detail_img;
+    this.start_at = start_at;
+    this.end_at = end_at;
   }
 }
 
